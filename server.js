0ed16b4e19ad7d7e23717241c5372ef70ff54a25
jmazrimas <joe.mazrimas@gmail.com>
2017-06-04 17:46:07 -0500

    remove es6 syntax

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 const http = require('http')  
 const port = 3000
 
-const requestHandler = (request, response) => {  
+const requestHandler = function (request, response) {  
   console.log(request.url)
   response.end('Hello from 2140!')
 }
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {  
+server.listen(port, function(err) {  
   if (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log('server is listening on '+port)
 })
