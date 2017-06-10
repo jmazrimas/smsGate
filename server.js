5fcaffe7833521c6f4ac96c3ae46156ac5965245
jmazrimas <joe.mazrimas@gmail.com>
2017-06-10 11:18:09 -0500

    correctly creating request for existing user

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
 var models = require('./server/models/index');
+var bodyParser = require('body-parser');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
   res.render('home');
@@ -20,13 +22,35 @@ app.get('/requests', function(req, res) {
 });
 
 app.post('/requests', function(req, res) {
-	models.request.create({ actioned: false, userId: 1 })
-  .then(function(request) {
-  	res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify(request));
-  });
+	var smsFrom = req.body.From.replace('+','');
+	var smsBody = req.body.Body;
+
+	models.user.findOne({
+	  where: {
+	    phone: smsFrom
+	  }
+	}).then(function(reqUser){
+		if (!reqUser) {
+			console.log('user not found');
+		} else {
+			console.log('user found!');
+			console.log(reqUser)
+			models.request.create({ actioned: false, userId: reqUser.id })
+			.then(function(newRequest) {
+				console.log(newRequest)
+			})
+
+		}
+	});
+	// models.request.create({ actioned: false, userId: 1 })
+ //  .then(function(request) {
+ //  	res.setHeader('Content-Type', 'application/json');
+	// 	res.send(JSON.stringify(request));
+ //  });
+ 	res.send('OK')
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
